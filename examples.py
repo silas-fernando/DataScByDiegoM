a250fbef096c2e4db1f6a02ee892c8d7b55db2f8
Silas-fp <silasf_primiani@hotmail.com>
2020-05-17 18:25:21 -0300

    8. Comparando gráfico de barras.

--- examples.py
+++ examples.py
@@ -1,10 +1,13 @@
 # Visualização de dados em Python
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 7, 1, 0]
+x1 = [1, 3, 5, 7, 9]
+y1 = [2, 3, 7, 1, 0]
 
-titulo = "Gráfico de barras"
+x2 = [2, 4, 6, 8, 10]
+y2 = [5, 1, 3, 7, 4]
+
+titulo = "Comparando gráficos de barras"
 eixox = "Eixo X"
 eixoy = "Eixo Y"
 
@@ -13,5 +16,8 @@ plt.title(titulo)
 plt.xlabel(eixox)
 plt.ylabel(eixoy)
 
-plt.bar(x,y)
+plt.bar(x1, y1, label = "Grupo 1")
+plt.bar(x2, y2, label = "Grupo 2")
+plt.legend()
+
 plt.show()
