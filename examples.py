0dec77e76a763f69af05591485f95de806166e32
Silas-fp <silasf_primiani@hotmail.com>
2020-05-18 02:02:50 -0300

    23. Estudo de caso: Bioinformática - comparando genomas.

--- examples.py
+++ examples.py
@@ -1,13 +1,31 @@
-# Boxplot - diagrama de caixa
-import matplotlib.pyplot as plt
-import random
+# 23. Estudo de caso: Bioinformática - comparando genomas parte 4
+entrada = open("human.fasta").read()
+saida = open("human.html","w")
 
-vetor = []
+cont = {}
 
-for i in range(20):
-	numero_aleatorio = random.randint(0,10)
-	vetor.append(numero_aleatorio)
+for i in ['A', 'T', 'C', 'G']:
+	for j in ['A', 'T', 'C', 'G']:
+		cont[i+j] = 0
 
-plt.boxplot(vetor)
-plt.title("Boxplot")
-plt.show()
+entrada = entrada.replace("\n","")
+
+for k in range(len(entrada)-1):
+	cont[entrada[k]+entrada[k+1]] += 1
+
+# html
+
+saida.write("<div>")
+
+i = 1
+for k in cont:
+	transparencia = cont[k]/max(cont.values())
+	saida.write("<div style='width:100px; border:1px solid #111; color:#fff; height:100px; float:left; background-color:rgba(0, 0, 0, "+str(transparencia)+"')>"+k+"</div>")
+
+	if i%4 == 0:
+		saida.write("<div style='clear:both'></div>")
+
+	i+=1
+
+saida.close()
+
